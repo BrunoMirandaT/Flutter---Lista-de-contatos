7ddaae95f62e01fd28df76d98bc4c2ce9a982021
brutusmt0811 <brutusmt0811@gmail.com>
2024-11-02 23:45:45 +0000

    Final

--- lib/menu.dart
+++ lib/menu.dart
@@ -4,12 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:myapp/contact_generator.dart';
 import 'package:myapp/database.dart';
-import 'package:simple_gradient_text/simple_gradient_text.dart';
-import 'package:prompt_dialog/prompt_dialog.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class menu extends ConsumerWidget {
