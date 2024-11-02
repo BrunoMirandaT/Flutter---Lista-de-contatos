ee0b5b04514b597ef2b13a6c9842f45595e8fd10
brutusmt0811 <brutusmt0811@gmail.com>
2024-11-02 23:46:41 +0000

    Final

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -7,7 +7,6 @@ import 'package:flutter/foundation.dart'
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 // ...
 
